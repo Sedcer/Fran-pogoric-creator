11a90a006b3f8fe7714f6b44d50d6ef841fb8940
Sedcer <129540947+Sedcer@users.noreply.github.com>
2024-03-14 14:51:34 +0100

    Add files via upload

--- core.js
+++ core.js
@@ -1,13 +1,14 @@
+window.addEventListener("load", function() {
     // Assuming randomEventChoices is an object containing arrays of random event choices
     // Modify this object and its structure according to your specific implementation
     
-    // Remove "core_element" from the array of random event choices related to floating objects
-    if (randomEventChoices.floating_objects && randomEventChoices.floating_objects.indexOf("core_element") !== -1) {
-        randomEventChoices.floating_objects.splice(randomEventChoices.floating_objects.indexOf("core_element"), 1);
+    // Add "reactor_core" to the array of random event choices related to Special
+    if (randomEventChoices.Special && randomEventChoices.Special.indexOf("reactor_core") === -1) {
+        randomEventChoices.Special.push("reactor_core");
     }
-
-    // Remove "reactor_core" from the array of random event choices related to reactor cores
-    if (randomEventChoices.reactor_cores && randomEventChoices.reactor_cores.indexOf("reactor_core") !== -1) {
-        randomEventChoices.reactor_cores.splice(randomEventChoices.reactor_cores.indexOf("reactor_core"), 1);
+    
+    // Remove "radiation" from the array of random event choices related to explosions
+    if (randomEventChoices.explosion && randomEventChoices.explosion.indexOf("radiation") !== -1) {
+        randomEventChoices.explosion.splice(randomEventChoices.explosion.indexOf("radiation"), 1);
     }
 });
